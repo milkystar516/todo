238fa78cdc6c66817ded141432787fc56af1a190
lim <milkystar516@gmail.com>
2026-03-17 22:58:33 +0900

    feature: Todo 클래스 생성

--- src/main/java/com/example/todo/Todo.java
+++ src/main/java/com/example/todo/Todo.java
@@ -1,5 +1,14 @@
 package com.example.todo;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
     private Long id;
     private String title;
