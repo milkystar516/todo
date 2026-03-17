726cb4b3e6646e8bac861f598bd464c312893f14
lim <milkystar516@gmail.com>
2026-03-17 23:34:51 +0900

    feature:todo 추가 구현

--- src/main/java/com/example/todo/TodoController.java
+++ src/main/java/com/example/todo/TodoController.java
@@ -1,12 +1,32 @@
 package com.example.todo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList; 
+import java.util.List; 
+import java.util.concurrent.atomic.AtomicLong; 
+import org.springframework.stereotype.Controller; 
+import org.springframework.ui.Model; 
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TodoController {
+    private final List<Todo> todos = new ArrayList<>();
+    private final AtomicLong idGenerator = new AtomicLong();
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("todos", todos);
         return "index";
+    }
+
+    @PostMapping("/add")
+    public String addTodo(@RequestParam String title) {
+        Todo todo = new Todo(
+            idGenerator.incrementAndGet(),
+            title,
+            false
+        );
+
+        todos.add(todo);
+        return "redirect:/";
     }
 }
