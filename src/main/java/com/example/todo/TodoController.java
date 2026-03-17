0f2bf6aba1219cac903642236cb66cb6257bdb18
lim <milkystar516@gmail.com>
2026-03-17 22:49:51 +0900

    feature:todocontroller 추가

---
+++ src/main/java/com/example/todo/TodoController.java
@@ -0,0 +1,12 @@
+package com.example.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TodoController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
