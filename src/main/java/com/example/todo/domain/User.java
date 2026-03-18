7335c7d005da1c89770a6f747e8728aec9441a3e
lim <milkystar516@gmail.com>
2026-03-19 03:46:10 +0900

    feature:user builder 추가

--- src/main/java/com/example/todo/domain/User.java
+++ src/main/java/com/example/todo/domain/User.java
@@ -24,4 +24,16 @@ public class User {
     private String password;
 
     private boolean enabled = false;
+
+    @Builder
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.enabled = false;
+    }
+
+    public void enable_user() {
+        this.enabled = true;
+    }
 }
