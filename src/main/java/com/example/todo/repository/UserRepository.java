beeede8bca0167e7b8b6410802040dced54bbd9f
lim <milkystar516@gmail.com>
2026-03-19 03:57:07 +0900

    feature:user 레포지터리 추가

--- src/main/java/com/example/todo/repository/UserRepository.java
+++ src/main/java/com/example/todo/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.example.todo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public class UserRepository {
-    
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
