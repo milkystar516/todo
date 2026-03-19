e4113a54566e214e55edb8d6f69620d332845880
lim <milkystar516@gmail.com>
2026-03-19 20:00:35 +0900

    bugfix:todo 컨트롤러를 interface로 변경

--- src/main/java/com/example/todo/repository/TodoRepository.java
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -5,6 +5,6 @@ import com.example.todo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public class TodoRepository extends JpaRepository<Todo, Long>{
+public interface TodoRepository extends JpaRepository<Todo, Long>{
     List<Todo> findByUser(User user);
 }
