e1e30bd577e0bfb943d6dd1729b836be613036b0
lim <milkystar516@gmail.com>
2026-03-19 17:51:02 +0900

    feature:회원정보 탐색 서비스 추가

--- src/main/java/com/example/todo/config/SecurityConfig.java
+++ src/main/java/com/example/todo/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.example.todo.config;
 
+import com.example.todo.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,17 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    @Profile("dev")
-    public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-            .csrf(csrf -> csrf.disable())
-            .formLogin(form -> form.disable())
-            .httpBasic(basic -> basic.disable());
-
-        return http.build();
-    }
+    
 
     @Bean
     public PasswordEncoder passwordEncoder() {
