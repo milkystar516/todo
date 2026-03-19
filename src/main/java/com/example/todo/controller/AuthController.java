4c0e308abba51b7d5512c958f2c21096f977be38
lim <milkystar516@gmail.com>
2026-03-19 15:07:01 +0900

    feature:회원가입 페이지 html 추가

---
+++ src/main/java/com/example/todo/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.example.todo.controller;
+
+import com.example.todo.service.SignupForm;
+import com.example.todo.service.SignupService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+    
+}
