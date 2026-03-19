0204a5c19e77476be4d88b7c71b5ee6a68141807
lim <milkystar516@gmail.com>
2026-03-19 19:48:47 +0900

    bugfix:todo 엔티티에 있는 오타 수정

--- src/main/java/com/example/todo/domain/Todo.java
+++ src/main/java/com/example/todo/domain/Todo.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class Todo {
     
     @Id
-    @GeneratedValue(strategy = GeneratedValue.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
